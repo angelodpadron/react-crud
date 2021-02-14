b374271b61f2cdf022f744ea3ebe1a9270dca6ed
Angelo Padron <padron891@gmail.com>
2021-02-14 20:50:56 -0300

    Finished basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Products from './admin/Products';
+import Main from './main/Main';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+
+            <BrowserRouter>
+              <Route path={'/'} exact component={Main}/>
+              <Route path={'/admin/products'} component={Products}/>
+            </BrowserRouter>
+            
+          
     </div>
+    
   );
 }
 
