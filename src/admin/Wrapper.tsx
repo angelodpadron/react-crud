d812a03bb5c943a2bd5de64e0418545a6ce7ecff
Angelo Padron <padron891@gmail.com>
2021-02-16 18:38:53 -0300

    Finished

--- src/admin/Wrapper.tsx
+++ src/admin/Wrapper.tsx
@@ -10,11 +10,20 @@ const Wrapper = (props: PropsWithChildren<any>) => {
             <div className="container-fluid">
                 <div className="row">                
                     <Menu />
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4" >
+                    <main className="container footer" >
                         {props.children}
                     </main>
                 </div>
             </div>
+            <footer className="text-muted bg-dark py-5">
+                <div className="container">
+                    <p className="float-end mb-1">
+                    <a href="#">Back to top</a>
+                    </p>
+                    <p className="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+                    <p className="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.0/getting-started/introduction/">getting started guide</a>.</p>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/admin/components/Menu.tsx
+++ src/admin/components/Menu.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 const Menu = () => {
     return(
-        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
             <div className="position-sticky pt-3">
               <ul className="nav flex-column">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
-                    Product
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita voluptates itaque eaque in deserunt, dolores quod ex esse alias, nostrum error quis omnis rem odio non. Quis sed natus aliquid.
                   </a>
+                  <a className="nav-link" aria-current="page" href="#">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus itaque distinctio, ullam dignissimos quas quo sit placeat in ea pariatur facere, alias temporibus, iure amet! Dolorem fuga totam corporis.
+                  </a>
+                  <a className="nav-link" aria-current="page" href="#">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit quasi ipsam asperiores est obcaecati quas quos optio eum. Tempora aperiam eum qui cumque perspiciatis voluptates corrupti fugit pariatur magni blanditiis.
+                  </a>
+
                 </li>
               </ul>
             </div>
